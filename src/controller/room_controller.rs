fb7c1f0559187e06f328e0c0d2cc40fc39247c5c
hhertout <hugues.hertout@gmail.com>
2024-06-26 20:50:10 +0200

    feat: adding uuid gen

--- src/controller/room_controller.rs
+++ src/controller/room_controller.rs
@@ -3,12 +3,15 @@ use std::sync::Arc;
 use redis::Connection;
 use serde_json::json;
 use tokio::sync::Mutex;
+use uuid::Uuid;
 use warp::reply::{json, Json};
 
-pub fn create_room(redis: Arc<Mutex<Connection>>) -> Json {
+pub fn create_room(_redis: Arc<Mutex<Connection>>) -> Json {
+    let id = Uuid::new_v4();
+
     return json(&json!({"status": "ok"}));
 }
 
-pub fn delete_room(redis: Arc<Mutex<Connection>>) -> Json {
+pub fn delete_room(_redis: Arc<Mutex<Connection>>) -> Json {
     return json(&json!({"status": "ok"}));
 }
